d418e651b21fdabe433c6b889f0753464ce721dc
xieshuangjie <472878808@qq.com>
2020-12-10 21:45:10 +0800

    新增页面

---
+++ src/views/project/subsisting/payment.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="components-container">付息首页</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/project/subsisting/principal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="components-container">本金首页</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
